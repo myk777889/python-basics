46f8a3d64685325744cb66603e6fa4db421e978d
myk777889 <myeshwanthkumar0@gmail.com>
2025-11-14 14:29:21 +0530

    Added variables example

---
+++ basics/02_variables.py
@@ -0,0 +1,11 @@
+# Basic Python Variables
+
+name = "Yeshwanth"
+age = 18
+is_student = True
+gpa = 7.27
+
+print("Name:", name)
+print("Age:", age)
+print("Student:", is_student)
+print("GPA:", gpa)
