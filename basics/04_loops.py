fa90a71c644ba59c98ef87d7db99361e9aa5a2fe
myk777889 <myeshwanthkumar0@gmail.com>
2025-11-14 14:45:31 +0530

    Added loops example

---
+++ basics/04_loops.py
@@ -0,0 +1,18 @@
+# Python Loops Example
+
+# For loop
+print("For Loop:")
+for i in range(5):
+    print("Number:", i)
+
+print("\nWhile Loop:")
+# While loop
+counter = 0
+while counter < 5:
+    print("Counter:", counter)
+    counter += 1
+
+print("\nLoop Through a List:")
+fruits = ["apple", "banana", "mango"]
+for fruit in fruits:
+    print(fruit)
