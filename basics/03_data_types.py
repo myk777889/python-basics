f45172328d9a408cac76d5d9c1511669fe2999c3
myk777889 <myeshwanthkumar0@gmail.com>
2025-11-14 14:31:09 +0530

    Added data types example

---
+++ basics/03_data_types.py
@@ -0,0 +1,17 @@
+# Python Data Types Example
+
+integer_value = 10
+float_value = 3.14
+string_value = "Hello AI"
+boolean_value = False
+list_value = [1, 2, 3, 4]
+tuple_value = (10, 20, 30)
+dict_value = {"name": "Yeshwanth", "age": 18}
+
+print("Integer:", integer_value)
+print("Float:", float_value)
+print("String:", string_value)
+print("Boolean:", boolean_value)
+print("List:", list_value)
+print("Tuple:", tuple_value)
+print("Dictionary:", dict_value)
