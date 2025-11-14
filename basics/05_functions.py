acf2d30485e846ffe234b5f55b772fdcaa975cb7
myk777889 <myeshwanthkumar0@gmail.com>
2025-11-14 14:48:48 +0530

    Added functions example

---
+++ basics/05_functions.py
@@ -0,0 +1,21 @@
+# Functions in Python
+
+
+def greet():
+    print("Hello, welcome to Python functions!")
+
+#  Parameters
+def add_numbers(a, b):
+    return a + b
+
+# Return value
+def get_message(name):
+    return f"Hello {name}, keep learning Python!"
+
+# Call functions
+greet()
+result = add_numbers(10, 20)
+print("Addition Result:", result)
+
+message = get_message("Yeshwanth")
+print(message)
